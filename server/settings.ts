5688e028272e1ac4d7b816399ea18f10d5833e54
dawbanc <dawsonxbancroft@gmail.com>
2025-01-24 16:21:44 -0800

    Added some comments

--- server/settings.ts
+++ server/settings.ts
@@ -1,15 +1,8 @@
 import { DeskThing, DataInterface, SettingsType } from "deskthing-server";
 
-/**
- * 
- * ----------- Settings ------------------
- * 
- * The following function is used to setup the settings for the app.
- * 
+/**The following function is used to setup the settings for the app.
  * This portion of the example will walk through the way you can define and add settings and ensure they are up-to-date with existing data.
- * 
  * It's usually a good idea to extract this 
- * 
  * @param Data - DataInterface | undefined
  */
 export const setupSettings = async (Data: DataInterface | null) => {
@@ -17,6 +10,9 @@ export const setupSettings = async (Data: DataInterface | null) => {
     const api_uri = await fetch('https://api.weather.gov/points/lat,long}');
     const { lat, long } = await api_uri.json();
 
+    // We require Latitude and Longitude so we can get data from nearby
+    // Temperature scale sets whether we will send celcius or freedom units to the deskthing client
+    // Wind speed scale sets whether we will send kilometers per hour or freedom units to the deskthing client
     const requiredSettings = [
       "latitude",
       "longitude",
@@ -24,14 +20,11 @@ export const setupSettings = async (Data: DataInterface | null) => {
       "wind_speed_scale"
     ];
 
-    // This checks if there are any settings missing
     const hasAllSettings = requiredSettings.every(
       (setting) => Data?.settings?.[setting]
     );
   
-    // If any of the settings are missing, then add them
     if (!hasAllSettings) {
-      // Defined the settings object. SettingsType is an interface of any setting type. This sets up the key-value pair of settings
       const Settings: { [key: string]: SettingsType } = {
         latitude: {
           label: "Latitude",
