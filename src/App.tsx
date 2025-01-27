bd951a5fcf448198e9b279cc68fc8f89acdd079f
dawbanc <dawsonxbancroft@gmail.com>
2025-01-26 21:04:24 -0800

    Initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -19,6 +19,7 @@ const App: React.FC = () => {
     return (
         <div className="bg-slate-800 w-screen h-screen flex justify-center items-center">
             <p className="font-bold text-5xl text-white">NWS-Weather</p>
+            
         </div>
 
     )
